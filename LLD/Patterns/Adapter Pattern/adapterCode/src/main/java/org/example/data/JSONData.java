a1abf46c010f2e697064ca5b2a3ac3308b05b52b
RituparnaPadira <65811601+RituparnaPadira@users.noreply.github.com>
2025-07-31 08:43:57 +0530

    Delete LLD/Patterns/Adapter Pattern directory

--- LLD/Patterns/Adapter Pattern/adapterCode/src/main/java/org/example/data/JSONData.java
+++
@@ -1,6 +0,0 @@
-package org.example.data;
-
-public class JSONData {
-    public String data = "fdsadfghjkghf";
-    public int keys = 2;
-}
--- LLD/Patterns/Adapter Pattern/adapterCode/src/main/java/org/example/data/XMLData.java
+++
@@ -1,6 +0,0 @@
-package org.example.data;
-
-public class XMLData {
-    public String data = "gdfghjk";
-    public int tags = 5;
-}
